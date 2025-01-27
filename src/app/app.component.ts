34097f619a9afa119581246b33548c4f0c23bfba
anshbegmirza <anshbegmirza@gmail.com>
2025-01-27 18:21:58 +0530

    completed test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-1';
+  user = {
+    name: 'test-name',
+    email: 'test@test.com',
+    phoneNumber: '+91 123456789',
+    age: '27',
+  };
+
+  showFields = false;
+
+  editUser() {
+    this.showFields = !this.showFields;
+    alert('You want to edit the user !');
+  }
+
+  updateUser() {
+    this.showFields = !this.showFields;
+
+    alert(`You have updated user.`);
+  }
 }
